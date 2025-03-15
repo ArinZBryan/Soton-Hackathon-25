82ecde32fda036c1d219faf5a7441edc4a9a0320
Arin Bryan <azb1g24@soton.ac.uk>
2025-03-15 17:37:57 +0000

    Multi-Tasking

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using Unity.VersionControl.Git;
 
 public class TaskManager : MonoBehaviour
 {
     public List<Task> tasks = new List<Task>();
-    public Vector3 currentTaskPosition;
-    public bool currentTaskWillInfect;
+    Vector3 currentTaskPosition;
+    bool currentTaskWillInfect;
     public float currentTaskDuration;
-    System.Random rng = new System.Random();
-    AIMovement AIMovement;
+    public AIMovement AIMovement;
+    System.Random rng;
     void getNewTask()
     {
-        Task currentTaskReference = tasks[rng.Next(0, tasks.Count - 1)];
+        int chosenTask = rng.Next(0, tasks.Count - 1); 
+        Task currentTaskReference = tasks[chosenTask];
+        Debug.Log(chosenTask);
 
         currentTaskPosition = currentTaskReference.position;
         currentTaskWillInfect = currentTaskReference.willInfect;
@@ -24,7 +27,10 @@ public class TaskManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AIMovement = gameObject.GetComponent<AIMovement>();
+        GameObject[] TP_objects = GameObject.FindGameObjectsWithTag("TaskPoint");
+        Debug.Log("Found " + TP_objects.Length.ToString() + " tasks");
+        for (int i = 0; i < TP_objects.Length; i++) { tasks.Add(TP_objects[i].GetComponent<Task>()); }
+        rng = new System.Random();
         getNewTask();
 
     }
@@ -33,7 +39,7 @@ public class TaskManager : MonoBehaviour
     {
         if (currentTaskDuration > 0)
         {
-            if ((currentTaskPosition - transform.position).magnitude < 0.5) { currentTaskDuration -= Time.fixedDeltaTime; }
+            if ((currentTaskPosition - transform.position).magnitude < 2) { currentTaskDuration -= Time.fixedDeltaTime; }
         }
         else
         {
